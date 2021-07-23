267470bea3e377f001fcaf23b1cd9234f69a9c6d
袁振 <zhen@yuanzhendeMacBook-Pro.local>
2021-07-23 18:38:45 +0800

    delete docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/zhen/Documents/01-工作文件/Support Team/技术文档/kuihua-ebook/src/css/custom.css"),
-  require("/Users/zhen/Documents/01-工作文件/Support Team/技术文档/kuihua-ebook/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
